bfe3418d0cc21e6be96d5d80a801de9e420f0032
Zaher Kassem <zaher.k1981@gmail.com>
2021-07-10 00:19:50 +0300

    random cities

--- src/api/openweathermapAPI.js
+++ src/api/openweathermapAPI.js
@@ -4,7 +4,7 @@ const httpRequests = {
     getTemperature(cityName, cbs, cbf)
     {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9cff733aee57cb05b63dd4f731c46bc4&units=metric`;
-        console.log(url);
+        // console.log(url);
         axios.get(url)
         .then(response => {
             // console.log({response});
